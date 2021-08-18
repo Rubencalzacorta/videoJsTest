919d3c3d776549c68c5d99b7f659693bfcd035da
Ruben Calzacorta <calzacorta.ruben@gmail.com>
2021-08-18 17:38:53 +0200

    working player

---
+++ index.js
@@ -0,0 +1,15 @@
+import videojs from 'video.js';
+import './node_modules/video.js/dist/video-js.css';
+
+var options = {};
+var player = videojs('my-player', options, function onPlayerReady() {
+  videojs.log('Your player is ready!');
+
+  // In this context, `this` is the player that was created by Video.js.
+  this.play();
+
+  // How about an event listener?
+  this.on('ended', function() {
+    videojs.log('Awww...over so soon?!');
+  });
+});
